4ef8f6a85c30c18d1e026dc9917259a53cb97c05
Ruslanbek <ruslanbek.tulqinov.01@bk.ru>
2024-05-11 05:21:54 +0500

    home/ dan '' ga o'tildi. endi sayt to'g'ri home pageda ochiladi

--- news_app/urls.py
+++ news_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/', article, name='home'),
+    path('', article, name='home'),
     path('world/', world_news, name='world'),
     path('sport/', sports_news, name='sport'),
     path('local/', local_news, name='local'),
